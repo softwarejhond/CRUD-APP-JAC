f3c85754835c15579d46066846762a9c27684628
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2023-02-07 01:34:53 -0500

    update

--- insert.php
+++ insert.php
@@ -1,13 +1,21 @@
 <?php
 include 'conexion.php';
 //START--  save information in variable get the inputs 
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$direccion = $_POST["direccion"]; 
-
+$tipoIdentificacion = $_POST["tipoIdentificacion"];
+$numeroIdentificacion = $_POST["numeroIdentificacion"];
+$lugarExpedicionDoc = $_POST["lugarExpedicionDoc"]; 
+$nombre = $_POST["nombre"]; 
+$apellidos = $_POST["apellidos"];
+$fechaNacimiento = $_POST["fechaNacimiento"];
+$edad = $_POST["edad"];
+$genero = $_POST["genero"];
+$direccion = $_POST["direccion"];
+$telefono = $_POST["telefono"];
+$email = $_POST["email"];
+$actividades = $_POST["actividades"];
 $dataTime = date("Y-m-d H:i:s");
 //END--  save information in variable get the inputs 
-$sql = "INSERT INTO estudents(tipoIdentificacion,numeroIdentificacion,lugarExpedicionDoc,nombre,apellidos,fechaNacimiento,edad,genero,direccion,telefono,email,actividades,fechaCreacionEst) VALUES ('$nombre','$correo','$direccion','$nombre','$apellidos','$fechaNacimiento','$edad','$genero','$direccion','$telefono','$email','$actividades','$dataTime')";					
+$sql = "INSERT INTO estudents(,tipoIdentificacion,numeroIdentificacion,lugarExpedicionDoc,nombre,apellidos,fechaNacimiento,edad,genero,direccion,telefono,email,actividades,fechaCreacionEst) VALUES ('$tipoIdentificacion','$numeroIdentificacion','$lugarExpedicionDoc','$nombre','$apellidos','$fechaNacimiento','$edad','$genero','$direccion','$telefono','$email','$actividades','$dataTime')";					
 $result=mysqli_query($conexion,$sql);
 
 if($result){
