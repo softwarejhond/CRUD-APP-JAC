ff4f5b79758d11f4fe561a46b1e32f5ef3b84827
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2023-02-07 01:52:26 -0500

    update utf8

--- conexion.php
+++ conexion.php
@@ -4,7 +4,7 @@ $username="agenciae";
 $password="q294g.#PwM9SYy";
 $database="agenciae_cartasJAC";
 $conexion=new mysqli($hostname,$username,$password,$database);
-mysqli_set_charset($con, 'utf8'); //Muy importante esta linea, guardara el contenido que contenga acentos de manera correcta configurando la bd con el UTF-8 spanis ci
+mysqli_set_charset($conexion, 'utf8'); //Muy importante esta linea, guardara el contenido que contenga acentos de manera correcta configurando la bd con el UTF-8 spanis ci
 if($conexion->connect_errno){
     echo 'El sistema esta presentando problemas';
 }
